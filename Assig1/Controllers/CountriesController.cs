bb170c9988a239991aada3ea744323376de30ba1
Maddumage Don Suneth Neranja <madsy019@mymail.unisa.edu.au>
2023-09-29 11:29:20 +0930

    - Displaying all country names and urls to the country page(no design yet)

--- Assig1/Controllers/CountriesController.cs
+++ Assig1/Controllers/CountriesController.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assig1.Data;
+using Assig1.Models;
+
 
 namespace Assig1.Controllers
 {
     public class CountriesController : Controller
     {
-        public IActionResult Index()
+        private readonly EnvDataContext _context;
+
+        //constructor to load the database 
+        public CountriesController(EnvDataContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+        /*        public IActionResult Index()
+                {
+                    return View();
+                }*/
+
+        //Index Action to display the list of countires
+        public async Task<IActionResult> Index()
+        {
+            var countries = await _context.Countries
+                .OrderBy(c => c.CountryName)
+                .ToListAsync();
+
+            return View(countries);
+
         }
     }
 }
