002ca196023ef08e0399a15a4348709c82f7fac1
Maddumage Don Suneth Neranja <madsy019@mymail.unisa.edu.au>
2023-10-01 17:22:42 +1030

    -Fix issue on diaplying Temperature data in Summary card in Details page

--- Assig1/Controllers/CitiesController.cs
+++ Assig1/Controllers/CitiesController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Assig1.Data;
+using Assig1.Models;
+using Assig1.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Assig1.Controllers
 {
     public class CitiesController : Controller
     {
-        public IActionResult Index()
+
+        private readonly EnvDataContext _Context;
+
+        public CitiesController(EnvDataContext context)
+        {
+            _Context = context;
+        }
+
+
+        public IActionResult Index(int countryID, string? searchTerms)
         {
             return View();
         }
