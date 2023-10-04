06d78fac0c1567958ca2b215953b9c4b003245f0
Maddumage Don Suneth Neranja <madsy019@mymail.unisa.edu.au>
2023-10-04 20:49:45 +1030

    -Create city details page and populate CityDetailsViewModel

--- Assig1/ViewModels/CityDetailsViewModel.cs
+++ Assig1/ViewModels/CityDetailsViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class CityDetailsViewModel
     {
+        //properties for the city details
+        public int CityId { get; set; }
+        public string? CityName { get; set; }
+        public string? CountryName { get; set; }
+        public string? RegionName { get; set; }
     }
 }
